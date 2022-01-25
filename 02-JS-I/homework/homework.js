8a588df10824964f40396846e066f168cb5dc41a
Bruno <brunoangelozzi1997@gmail.com>
2022-01-25 03:12:00 -0300

    finish homework 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'bruno';
 const nuevoNum = 1;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
